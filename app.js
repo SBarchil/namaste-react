423db62c1bf967d58fa278e096b05a5b6e604227
Said barchil <barchil@gmail.com>
2025-01-29 16:03:24 -0500

    statting react

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "myH1" }, " Here is my 1st H1"),
-    React.createElement("h2", { id: "myH2" }, " Here is my 1st H2"),
-  ]),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    React.createElement("h1", { id: "myH2" }, " Here is my second H1")
-  ),
-]);
-
-const heading = React.createElement("h1", { id: "myDiv" }, "Hello World ");
+const heading = <h1 id="myheading"> Namaste React ! </h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
